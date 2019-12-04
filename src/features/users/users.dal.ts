67f00cf3b69a7773162b5f1ed27a9abfe58c9e4f
Maciej Zadrożny <contact@mzadrozny.pl>
2019-12-04 01:38:58 +0100

    dal update

--- src/features/users/users.dal.ts
+++ src/features/users/users.dal.ts
@@ -1,6 +1,29 @@
-import { pool } from "../../database";
 import { Users } from "./users.entity";
 
 export const getUsersData = () => {
-  return Users.find();
+  return Users.find({
+    order: {
+      name: "ASC",
+      id: "DESC"
+    }
+  });
+}
+
+export const getUserById = (id: string) => {
+  return Users.findOne(id);
+}
+
+export const postUserData = async (userData: Users) => {
+  const user = await Users.create(userData);
+  return Users.save(user);
+}
+
+export const putUserByIdData = async (userData: Users) => {
+  const user = await Users.findOne(userData.id);
+  Users.merge(user, userData);
+  return Users.save(user);
+}
+
+export const deleteUserById = (id: string) => {
+  return Users.delete(id);
 }
